7982bcc7516e78fbe933da091d31f77738d2bd43
hamzaMeski <meskihamza5@gmail.com>
2024-11-03 16:21:06 +0100

    Survey mapper setup

---
+++ BACKEND/src/main/java/com/SurveyManager/BACKEND/mapper/SurveyMapper.java
@@ -0,0 +1,29 @@
+package com.SurveyManager.BACKEND.mapper;
+
+import com.SurveyManager.BACKEND.dto.request.SurveyRequestDTO;
+import com.SurveyManager.BACKEND.dto.response.SurveyResponseDTO;
+import com.SurveyManager.BACKEND.entity.Survey;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring", uses = {OwnerMapper.class})
+public interface SurveyMapper {
+    @Mapping(target = "owner", source = "owner")
+    @Mapping(target = "editions", source = "editions")
+    SurveyResponseDTO toResponseDTO(Survey survey);
+    
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "owner.id", source = "ownerId")
+    @Mapping(target = "editions", ignore = true)
+    Survey toEntity(SurveyRequestDTO requestDTO);
+    
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "editions", ignore = true)
+    void updateEntity(@MappingTarget Survey survey, SurveyRequestDTO requestDTO);
+} 
