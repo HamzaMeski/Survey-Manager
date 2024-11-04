5689d0127dace22fe2270335af6a1a44dac5fafb
hamzaMeski <meskihamza5@gmail.com>
2024-11-04 14:57:48 +0100

    Question repository setup

--- BACKEND/src/main/java/com/SurveyManager/BACKEND/repository/SubjectRepository.java
+++ BACKEND/src/main/java/com/SurveyManager/BACKEND/repository/SubjectRepository.java
@@ -2,6 +2,8 @@ package com.SurveyManager.BACKEND.repository;
 
 import com.SurveyManager.BACKEND.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -10,4 +12,10 @@ public interface SubjectRepository extends JpaRepository<Subject, Long> {
     List<Subject> findBySurveyEditionIdAndParentSubjectIsNull(Long surveyEditionId);
     List<Subject> findByParentSubjectId(Long parentId);
     boolean existsByTitleAndSurveyEditionId(String title, Long surveyEditionId);
-} 
+
+    @Query("SELECT COUNT(s) FROM Subject s WHERE " +
+            "((:parentPath = '') AND s.parentSubject IS NULL) OR " +
+            "((:parentPath != '') AND s.parentSubject.path = :parentPath) " +
+            "AND s.level = :level")
+    long countByParentPathAndLevel(@Param("parentPath") String parentPath, @Param("level") Integer level);
+}
