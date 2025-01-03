817affa2f70b2cef7aead9149601c214d7328dc6
Hamza Meski <meskihamza5@gmail.com>
2025-01-03 14:17:20 +0100

    fixing issues

--- FRONTEND/src/app/features/edition/content-panel/content-panel.component.ts
+++ FRONTEND/src/app/features/edition/content-panel/content-panel.component.ts
@@ -1,7 +1,6 @@
-import {Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {SubjectResponse} from '../../../models/subject.interface';
 import {QuestionResponse} from '../../../models/question.interface';
-import {QuestionService} from '../../../core/services/question.service';
 import {CommonModule} from '@angular/common';
 import {QuestionListComponent} from './question/question-list.component';
 import {AnswerListComponent} from './answer/answer-list.component';
